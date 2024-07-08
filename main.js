4f5d4272d94b8fe4b8d3985e773aa553d6b8f3af
Yann Klein <yann.klein@me.com>
2024-07-08 20:14:16 +0900

    add popup to trips

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import mapboxgl from 'mapboxgl';
 
-const randomColorGen = () => "#" + Math.floor(Math.random() * 0xFFFFFF).toString(16);
- 
+const randomColorGen = () =>
+  '#' + Math.floor(Math.random() * 0xffffff).toString(16);
+
 mapboxgl.accessToken =
   'pk.eyJ1IjoieWFubmx1Y2tsZWluIiwiYSI6ImNqcnZmeHQwaDAxb2o0NGx2bG1tOWgwNGIifQ.q4zhKOCoH7nDIJNm88leXg';
 
@@ -17,6 +18,7 @@ map.on('load', () => {
     map.addSource(`bicycle-trip-${i}`, {
       type: 'geojson',
       data: `./bicycle-trips/bicycle-trip-${i}.geojson`,
+      lineMetrics: true,
     });
 
     map.addLayer({
@@ -24,9 +26,62 @@ map.on('load', () => {
       type: 'line',
       source: `bicycle-trip-${i}`,
       paint: {
-        'line-width': 3,
-        'line-color': randomColorGen(),
+        'line-width': 4,
+        'line-gradient': [
+          'interpolate',
+          ['linear'],
+          ['line-progress'],
+          0,
+          '#7b4397',
+          1,
+          '#dc2430',
+        ],
       },
+    });
+
+    // Create a popup, but don't add it to the map yet.
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+    });
+
+    map.on('mouseenter', `bicycle-trip-layer-${i}`, (e) => {
+      // Change the cursor style as a UI indicator.
+      map.getCanvas().style.cursor = 'pointer';
+
+      // Copy coordinates array.
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const description = e.features[0].properties.Name;
+      console.log(description);
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      if (['mercator', 'equirectangular'].includes(map.getProjection().name)) {
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+      }
+
+      // Populate the popup and set its coordinates
+      // based on the feature found.
+      const midCoord = coordinates[Math.round(coordinates.length / 2)];
+      const [title, info] = description.split(" | ");
+      const popupElement = `
+        <div class="popup">
+          <i class="icon fa-solid fa-person-biking"></i>
+          <h4>${title}</h4>
+          <p>${info}</p>
+        </div>
+      `;
+      popup.setLngLat(midCoord).setHTML(popupElement).addTo(map);
+    });
+
+    map.on('mouseleave', `bicycle-trip-layer-${i}`, () => {
+      setTimeout(() => {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+      }, 1000);
     });
   }
 });
