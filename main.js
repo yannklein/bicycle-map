4b18a361f8bd5dda8bc78de8544acdbbb9616e6d
Yann Klein <yann.klein@me.com>
2024-07-18 15:16:13 +0900

    add route to be cycled

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ import mapboxgl from 'mapboxgl';
 // const randomColorGen = () =>
 //   '#' + Math.floor(Math.random() * 0xffffff).toString(16);
 
-
 const initMap = () => {
   mapboxgl.accessToken =
     'pk.eyJ1IjoieWFubmx1Y2tsZWluIiwiYSI6ImNqcnZmeHQwaDAxb2o0NGx2bG1tOWgwNGIifQ.q4zhKOCoH7nDIJNm88leXg';
@@ -24,9 +23,15 @@ const initMap = () => {
     let routeIndex = 0;
     while (true) {
       try {
-        const response = await fetch(`./bicycle-trips/bicycle-trip-${routeIndex}.geojson`);
+        const response = await fetch(
+          `./bicycle-trips/bicycle-trip-${routeIndex}.geojson`,
+        );
         if (response.status === 404) {
-          throw new Error(`End of routes: route ${routeIndex} not found, loaded ${routeIndex - 1}`);
+          throw new Error(
+            `End of routes: route ${routeIndex} not found, loaded ${
+              routeIndex - 1
+            }`,
+          );
         }
         const data = await response.json();
         addBicycleRoute(map, data, routeIndex);
@@ -44,32 +49,53 @@ const initMap = () => {
 };
 
 const addBicycleRoute = (map, data, routeIndex) => {
+
+  const pending = data.status === 'pending';
+
   map.addSource(`bicycle-trip-${routeIndex}`, {
     type: 'geojson',
     data: `./bicycle-trips/bicycle-trip-${routeIndex}.geojson`,
     lineMetrics: true,
   });
 
+  const layerPaint = {
+    'line-width': 8,
+    'line-gradient': [
+      'interpolate',
+      ['linear'],
+      ['line-progress'],
+      0,
+      '#7b4397',
+      1,
+      '#dc2430',
+    ],
+  };
+
+  const layerLayout = {
+    'line-cap': 'round',
+    'line-join': 'round',
+  };
+
+  if (pending) {
+    layerPaint['line-width'] = 4;
+    layerPaint['line-dasharray'] = [0.5, 1.5];
+    layerPaint['line-gradient'] = [
+      'interpolate',
+      ['linear'],
+      ['line-progress'],
+      0,
+      '#777777',
+      1,
+      '#777777',
+    ];
+  }
+
   map.addLayer({
     id: `bicycle-trip-layer-${routeIndex}`,
     type: 'line',
-    layout: {
-      'line-cap': 'round',
-      'line-join': 'round',
-    },
+    layout: layerLayout,
     source: `bicycle-trip-${routeIndex}`,
-    paint: {
-      'line-width': 8,
-      'line-gradient': [
-        'interpolate',
-        ['linear'],
-        ['line-progress'],
-        0,
-        '#7b4397',
-        1,
-        '#dc2430',
-      ],
-    },
+    paint: layerPaint,
   });
 
   const coords = data.features
@@ -78,7 +104,7 @@ const addBicycleRoute = (map, data, routeIndex) => {
 
   const markerContent = document.createElement('div');
   markerContent.innerHTML = `
-        <span class="marker"><i class="icon fa-solid fa-person-biking"></i></span>
+        <span class="marker ${pending ? 'pending' : ''}"><i class="icon fa-solid fa-person-biking"></i></span>
       `;
   const [title, info] = data.name.split(' | ');
   const instaLinks = data?.instaLinks || [];
@@ -86,15 +112,16 @@ const addBicycleRoute = (map, data, routeIndex) => {
 
   const popupElement = `
         <div class="popup">
-          <i class="icon fa-solid fa-person-biking"></i>
+          <i class="icon ${pending ? 'pending' : ''} fa-solid fa-person-biking"></i>
           <div>
             <h4>${title}</h4>
-            <div class="info" >
+            <div class="info">
               <span><i class="fa-regular fa-clock"></i> ${days}</span>
               <span><i class="fa-solid fa-route"></i> ${distance}</span>
             </div>
           </div>
           <div>
+            ${pending ? 'Not cycled yet' : ''}
             ${instaLinks.reduce(
               (acc, link) =>
                 `${acc}<a target="_blank" class="insta-link" href=${link}><i class="fa-brands fa-square-instagram"></i></a>`,
@@ -106,7 +133,6 @@ const addBicycleRoute = (map, data, routeIndex) => {
   const popup = new mapboxgl.Popup({
     closeButton: false,
   }).setHTML(popupElement);
-
 
   const marker = new mapboxgl.Marker(markerContent)
     .setLngLat(coords[Math.floor(coords.length / 2)])
